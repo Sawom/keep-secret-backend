e624bfc36d443ede0b21492b78e143dbbb4fa3c2
Sawom <asawom250@gmail.com>
2026-09-06 11:11:19 +0600

    auth module setup and register dto

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CryptoModule } from './modules/crypto/crypto.module';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [CryptoModule, PrismaModule],
+  imports: [CryptoModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/modules/auth/dto/register.dto.ts
@@ -0,0 +1,25 @@
+//**  ক্লায়েন্ট থেকে ডাটা আসার সময় তা ভ্যালিডেট করার জন্য আমরা DTO ব্যবহার করি। */
+
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class RegisterDto {
+    // ইউজার নাম খালি রাখা যাবে না এবং স্ট্রিং হতে হবে
+    @IsString()
+    @IsNotEmpty({ message: 'Name is required' })
+    name: string;
+
+    // সঠিক ইমেইল ফরম্যাট চেক করবে
+    @IsEmail({}, { message: 'Invalid email address' })
+    @IsNotEmpty({ message: 'Email is required' })
+    email: string;
+
+    // পাসওয়ার্ড সর্বনিম্ন ৬ ক্যারেক্টার হতে হবে
+    @IsString()
+    @MinLength(6, { message: 'Password must be at least 6 characters long' })
+    password: string;
+
+    // কনফার্ম পাসওয়ার্ড ফিল্ড
+    @IsString()
+    @IsNotEmpty({ message: 'Confirm password is required' })
+    confirmPassword: string;
+}
