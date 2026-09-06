80b3e2d00b1c6e31a8bbf694141f64a72bb8514e
Sawom <asawom250@gmail.com>
2026-09-06 20:28:33 +0600

    add to module

--- src/modules/notebook/notebook.module.ts
+++ src/modules/notebook/notebook.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { NotebookService } from './notebook.service';
 import { NotebookController } from './notebook.controller';
+import { PrismaModule } from './../prisma/prisma.module';
+
+/**
+ * NotebookModule: নোটবুক সম্পর্কিত Controller, Service এবং Prisma Dependency একসাথে যুক্ত করে।
+ */
 
 @Module({
+  imports: [PrismaModule],
   controllers: [NotebookController],
   providers: [NotebookService],
+  exports: [NotebookService], // অন্য কোনো মডিউলে (যেমন: NoteModule) প্রয়োজন হলে যেন ব্যবহার করা যায়
 })
-export class NotebookModule {}
+
+export class NotebookModule { }
