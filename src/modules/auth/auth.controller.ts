edab1ea5f563fd0eb143c983e55fb99766273122
Sawom <asawom250@gmail.com>
2026-09-06 14:22:07 +0600

    route setup in controller, register in auth module

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,46 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ForgotPasswordDto, ResetPasswordDto } from './dto/forgot-password.dto';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
 
+// Base Route: /auth
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    /**
+     * [POST] /auth/register
+     */
+    @Post('register')
+    async register(@Body() dto: RegisterDto) {
+        return this.authService.register(dto);
+    }
+
+    /**
+     * [POST] /auth/login
+     */
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    async login(@Body() dto: LoginDto) {
+        return this.authService.login(dto);
+    }
+
+    /**
+     * [POST] /auth/forgot-password
+     */
+    @HttpCode(HttpStatus.OK)
+    @Post('forgot-password')
+    async forgotPassword(@Body() dto: ForgotPasswordDto) {
+        return this.authService.forgotPassword(dto);
+    }
+
+    /**
+     * [POST] /auth/reset-password
+     */
+    @HttpCode(HttpStatus.OK)
+    @Post('reset-password')
+    async resetPassword(@Body() dto: ResetPasswordDto) {
+        return this.authService.resetPassword(dto);
+    }
+}
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET || 'super-secret-jwt-key',
+      signOptions: { expiresIn: '7d' }, // টোকেন মেয়াদ ৭ দিন
+    }),
+  ],
+  controllers: [AuthController],
   providers: [AuthService],
-  controllers: [AuthController]
+  exports: [AuthService],
 })
-export class AuthModule {}
+
+export class AuthModule { }
