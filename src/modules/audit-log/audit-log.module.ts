547cb8147a9d7b7e56fb8e3c805f988e19e00d68
Sawom <asawom250@gmail.com>
2026-09-07 13:07:12 +0600

    audit log module done

--- src/modules/audit-log/audit-log.module.ts
+++ src/modules/audit-log/audit-log.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuditLogService } from './audit-log.service';
 import { AuditLogController } from './audit-log.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [AuditLogController],
   providers: [AuditLogService],
+  exports: [AuditLogService], // অন্য মডিউলে ইনজেক্ট করার জন্য এক্সপোর্ট
 })
-export class AuditLogModule {}
+
+export class AuditLogModule { }
