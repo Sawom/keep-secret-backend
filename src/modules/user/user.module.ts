b264c01861aeed304632c2fb95b50b364974fdf9
Sawom <asawom250@gmail.com>
2026-09-07 23:34:44 +0600

    use module added

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
+import { PassportModule } from '@nestjs/passport';
+import { AuditLogModule } from '../audit-log/audit-log.module';
 import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [
+    PrismaModule,
+    AuditLogModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [UserController],
   providers: [UserService],
+  exports: [UserService],
 })
-export class UserModule {}
+
+export class UserModule { }
