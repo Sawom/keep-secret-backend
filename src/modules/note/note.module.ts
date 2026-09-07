97e6ac3157a1d9ac0d0d5a6cb41be96f0b12be7c
Sawom <asawom250@gmail.com>
2026-09-07 09:59:09 +0600

    module registered

--- src/modules/note/note.module.ts
+++ src/modules/note/note.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { NoteService } from './note.service';
 import { NoteController } from './note.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
+  imports: [
+    PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   controllers: [NoteController],
   providers: [NoteService],
+  exports: [NoteService],
 })
-export class NoteModule {}
+
+export class NoteModule { }
