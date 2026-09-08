c38f9a7f5091b3ad89243c8968075dba96175a52
Sawom <asawom250@gmail.com>
2026-09-08 21:29:48 +0600

    fix: switch moduleResolution to bundler and ESNext

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import { AppModule } from '../src/app.module';
 import express from 'express';
+import { AppModule } from '../src/app.module';
 
 const server = express();
 let cachedServer: any;
