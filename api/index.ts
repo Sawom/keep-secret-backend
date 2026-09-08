ebdb835ce7253bd2b472c45fe4278ef26227ccbf
Sawom <asawom250@gmail.com>
2026-09-08 23:31:09 +0600

    fix: final vercel deployment fix

--- api/index.ts
+++ api/index.ts
@@ -1,28 +1,29 @@
-import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 
 const server = express();
-let appPromise: Promise<any>;
+let cachedServer: any;
 
 async function bootstrap() {
-    const { AppModule } = await import('../src/app.module');
-    const app = await NestFactory.create(
-        AppModule,
-        new ExpressAdapter(server),
-    );
-    app.enableCors({
-        origin: '*',
-        credentials: true,
-    });
-    await app.init();
-    return server;
+    if (!cachedServer) {
+        const { NestFactory } = await import('@nestjs/core');
+        const { AppModule } = await import('../src/app.module.js');
+
+        const app = await NestFactory.create(
+            AppModule,
+            new ExpressAdapter(server),
+        );
+        app.enableCors({
+            origin: '*',
+            credentials: true,
+        });
+        await app.init();
+        cachedServer = server;
+    }
+    return cachedServer;
 }
 
 export default async function handler(req: any, res: any) {
-    if (!appPromise) {
-        appPromise = bootstrap();
-    }
-    const appServer = await appPromise;
+    const appServer = await bootstrap();
     return appServer(req, res);
 }
