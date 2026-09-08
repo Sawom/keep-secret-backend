627ccd0d70aeb8273ca3cbc4600eb5a2312ef375
Sawom <asawom250@gmail.com>
2026-09-09 01:01:04 +0600

    fix: make all imports dynamic in api/index.ts to avoid ERR_REQUIRE_ESM

--- api/index.ts
+++ api/index.ts
@@ -1,22 +1,24 @@
-import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
-
-const server = express();
 let cachedServer: any;
 
 async function bootstrap() {
     if (!cachedServer) {
+        // সব ইমপোর্ট ডাইনামিকালি ইমপোর্ট করা হলো যাতে ERR_REQUIRE_ESM না আসে
+        const express = (await import('express')).default;
+        const { ExpressAdapter } = await import('@nestjs/platform-express');
         const { NestFactory } = await import('@nestjs/core');
         const { AppModule } = await import('../src/app.module.js');
 
+        const server = express();
         const app = await NestFactory.create(
             AppModule,
             new ExpressAdapter(server),
         );
+
         app.enableCors({
             origin: '*',
             credentials: true,
         });
+
         await app.init();
         cachedServer = server;
     }
