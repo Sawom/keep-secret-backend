e584f0fe6d587b1f45fd5951985272041519ac24
Sawom <asawom250@gmail.com>
2026-09-08 21:49:15 +0600

    fix: successful build with bundler module resolution

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import express from 'express';
 import { AppModule } from '../src/app.module';
+import express from 'express';
 
 const server = express();
 let cachedServer: any;
